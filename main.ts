fb1886ab960cda4b6824c1e862c5fb33adc96cb1
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-03-18 14:11:48 +0900

    ts

--- main.ts
+++ main.ts
@@ -1,28 +1,34 @@
 import { serve } from "https://deno.land/std@0.179.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.179.0/http/file_server.ts";
-import { renderFileToString } from "https://deno.land/x/dejs@0.10.3/mod.ts";
+import {
+  Params,
+  renderFileToString,
+} from "https://deno.land/x/dejs@0.10.3/mod.ts";
 import { parse } from "https://deno.land/std@0.180.0/encoding/csv.ts";
 
-const renderPage = async (tpl, params) => {
+async function renderPage(tpl: string, params: Params = {}) {
   const body = await renderFileToString(`${Deno.cwd()}/${tpl}`, params);
   return new Response(body, {
     headers: { "content-type": "text/html" },
   });
-};
+}
 
-serve(async (req) => {
+async function index() {
+  const data = await Deno.readTextFile("boring.csv");
+  const csv = parse(data, { skipFirstRow: true });
+  const borings = csv;
+  return { borings };
+}
+
+async function handler(req: Request) {
   const { pathname } = new URL(req.url);
   const path = `${req.method} ${pathname}`;
-  const router = new Map();
-  router.set("GET /", async () => {
-    const data = await Deno.readTextFile("boring.csv");
-    const csv = await parse(data, { skipFirstRow: true });
-    const borings = csv;
-    return await renderPage("index.ejs", { borings });
-  });
-  const handler = router.get(path);
-  if (handler) {
-    return await handler();
+
+  switch (path) {
+    case "GET /":
+      return renderPage("index.ejs", await index());
   }
   return serveDir(req, { fsRoot: "./static/" });
-});
+}
+
+serve(handler);
